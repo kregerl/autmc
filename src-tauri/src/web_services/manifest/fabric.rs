7801dcf88082f4f006673d64f66057f3563604c5
kregerl <loucaskreger@gmail.com>
2023-01-23 21:50:57 -0500

    Added fabric profile struct for deserializing the /profile/json endpoint

--- src-tauri/src/web_services/manifest/fabric.rs
+++ src-tauri/src/web_services/manifest/fabric.rs
@@ -1,15 +1,8 @@
 use serde::Deserialize;
 
+use crate::{consts::FABRIC_BASE_URL, web_services::downloader::download_json_object, state::resource_manager::ManifestResult};
 
-
-// #[derive(Debug, Deserialize)]
-// pub struct FabricGameVersion {
-//     pub version: String,
-//     stable: bool,
-// }
-
-// #[derive(Debug, Deserialize)]
-// pub struct FabricGameManifest(pub Vec<FabricGameVersion>);
+use super::vanilla::LaunchArguments;
 
 #[derive(Debug, Deserialize)]
 pub struct FabricLoaderVersion {
@@ -22,3 +15,30 @@ pub struct FabricLoaderVersion {
 
 #[derive(Debug, Deserialize)]
 pub struct FabricLoaderManifest(pub Vec<FabricLoaderVersion>);
+
+#[derive(Debug, Deserialize)]
+pub struct FabricLibrary {
+    name: String,
+    url: String
+}
+
+#[derive(Debug, Deserialize)]
+pub struct FabricProfile {
+    id: String,
+    #[serde(rename = "inheritsFrom")]
+    inherits_from: String,
+    #[serde(rename = "releaseTime")]
+    release_time: String,
+    time: String,
+    #[serde(rename = "type")]
+    version_type: String,
+    #[serde(rename = "mainClass")]
+    main_class: String,
+    arguments: LaunchArguments,
+    libraries: Vec<FabricLibrary>
+}
+
+pub async fn download_fabric_profile(minecraft_version: &str, fabric_version: &str) -> ManifestResult<FabricProfile> {
+    let url = format!("{}/versions/loader/{}/{}/profile/json", FABRIC_BASE_URL, minecraft_version, fabric_version);
+    Ok(download_json_object::<FabricProfile>(&url).await?)
+}
